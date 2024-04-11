11f16e7ca06433d296e4f3c9926c77995b97ab6e
bharathrajb6 <bharathrajb26@gmail.com>
2024-04-11 15:13:51 +0530

    Initial Commit

---
+++ Pharmacy-App/src/test/java/com/example/PharmacyApp/PharmacyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PharmacyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
