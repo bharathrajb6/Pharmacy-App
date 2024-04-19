a8dc36aebf0a2a6759fc1506b1d748f1af720f2a
bharathrajb6 <bharathrajb26@gmail.com>
2024-04-19 10:24:32 +0530

    Localization of error and warning messages

---
+++ Pharmacy-App/src/main/java/com/example/PharmacyApp/messages/CommonInformation.java
@@ -0,0 +1,1 @@
+package com.example.PharmacyApp.messages;public class CommonInformation {    public static final String ERROR = "Error";    public static final String INFO = "Info";    public static final String WARNING = "Warning";    public static final String ID_LENGTH_EXCEPTION = "Length should be from 3 to 16";}
